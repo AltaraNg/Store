9b7fd6f27e5aeda31d9b8103f6e7621c7773477e
AltaraNg <accounts@altaracredit.com>
2018-04-27 09:32:35 +0100

    upd

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ var app = new Vue({
             app.purchase.product_price = app.product_price;
             app.purchase.product_name = app.product_name;
            
-            app.purchase.repaymt =  Math.floor( (app.product_price - ( Math.floor((0.4*app.product_price)*100)/100)/12)*100)/100;
+            app.purchase.repaymt =   Math.floor( ((app.product_price - ((Math.floor((0.4*app.product_price)/100))*100))/12)/100)*100;
             var formData = app.toFormData(app.purchase);
             console.log(app.purchase)
             axios.post("https://altara-api.herokuapp.com/api.php?action=purchase", formData)
